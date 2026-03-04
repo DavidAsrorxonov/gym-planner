a4cc3a5e27edef4d54edeef6a056244b9eea9496
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-04 14:47:31 +0900

    feat: Add new routes for onboarding, authentication, account, and profile pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
