6f5a204030a593092f87b78c3a39ed90b75085e5
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-04 15:39:16 +0900

    feat: Integrate Neon authentication by adding `NeonAuthUIProvider` and creating an auth client.

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import Auth from "./pages/Auth";
 import Account from "./pages/Account";
 import Profile from "./pages/Profile";
 import Navbar from "./components/layout/navbar";
+import { NeonAuthUIProvider } from "@neondatabase/neon-js/auth/react";
+import { authClient } from "./lib/auth";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/auth/:pathname" element={<Auth />} />
-            <Route path="/account/:pathname" element={<Account />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <NeonAuthUIProvider authClient={authClient}>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-1">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/onboarding" element={<Onboarding />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/auth/:pathname" element={<Auth />} />
+              <Route path="/account/:pathname" element={<Account />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </NeonAuthUIProvider>
   );
 };
 
